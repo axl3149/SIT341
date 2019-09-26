cd1856be1a6da579641d23e8d42d08a71c21c9be
Piumi Fernando <pwarnaku@deakin.edu.au>
2019-09-26 14:13:30 +1000

    quick fix

--- safetyLab/safetyLab/StartPage.xaml.cs
+++ safetyLab/safetyLab/StartPage.xaml.cs
@@ -52,6 +52,7 @@ namespace safetyLab
             InitializeComponent();
             Children.Clear();
 
+
             mainList.ItemsSource = chemicalNames;
             mainList.SeparatorColor = Color.Black;
             mainList.HorizontalOptions = LayoutOptions.Center;
@@ -78,8 +79,10 @@ namespace safetyLab
             //SEARCH RESULTS
             resultsContent.Content = mainList;
             resultsContent.Title = "Search";
-            resultsContent.IconImageSource = "search_icon.png";
-            Children.Add(resultsContent);
+           
+
+
+            // resultsContent.IconImageSource = "search_icon.png";
 
             //FAVOURITES
             favouritesList.ItemsSource = favourites;
@@ -88,35 +91,39 @@ namespace safetyLab
 
             favouritesContent.Title = "Favourites";
             favouritesContent.Content = favouritesList;
-            favouritesContent.IconImageSource = "favourites_icon.png";
-            Children.Add(favouritesContent);
+            //favouritesContent.IconImageSource = "favourites_icon.png";
 
             //RECENTS
             recentsContent.Title = "Recents";
             recentsContent.Content = recentsList;
-            recentsContent.IconImageSource = "recents_icon.png";
+           // recentsContent.IconImageSource = "recents_icon.png";
+
+            Children.Add(resultsContent);
+            Children.Add(favouritesContent);
+
             Children.Add(recentsContent);
 
             //MAPS
             mapTabbed.Title = "Maps";
             mapTabbed.BarBackgroundColor = Color.FromRgb(66, 175, 178);
-            mapTabbed.IconImageSource = "map_icon.png";
 
+            //mapTabbed.IconImageSource = "map_icon.png";
+            
             StackLayout mapStackBurwood = new StackLayout();
             Image burwoodMap = new Image { Source = "deakin_burwood.jpg" };
-            Button security = new Button { Text = "Deakin Security", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
+            Button security = new Button { Text = "Deakin Security", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
             security.Clicked += (sender, e) => SecurityClicked();
             mapStackBurwood.Children.Add(security); 
 
-            Button emergency = new Button { Text = "Emergency Service (000) ", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
+            Button emergency = new Button { Text = "Emergency Service (000) ", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
             emergency.Clicked += (sender, e) => EmergencyClicked();
             mapStackBurwood.Children.Add(emergency);
 
-            Button medical = new Button { Text = "Deakin Medical (Building B)", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
+            Button medical = new Button { Text = "Deakin Medical (Building B)", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
             medical.Clicked += (sender, e) => MedicalClicked();
             mapStackBurwood.Children.Add(medical);
 
-            Button hospital = new Button { Text = "Box Hill Hospital", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
+            Button hospital = new Button { Text = "Box Hill Hospital", HorizontalOptions = LayoutOptions.CenterAndExpand, VerticalOptions = LayoutOptions.CenterAndExpand, FontSize = 24 };
             //hospital.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(() => { HospitalClicked(); }), NumberOfTapsRequired = 1 });
             hospital.Clicked += (sender, e) => HospitalClicked();
             mapStackBurwood.Children.Add(hospital);
