94e9b0475e78997f93e16457c1b87b988912fcc1
Piumi Fernando <pwarnaku@deakin.edu.au>
2019-10-14 12:08:56 +1100

    updated buttons

--- safetyLab/safetyLab.Android/Resources/Resource.designer.cs
+++ safetyLab/safetyLab.Android/Resources/Resource.designer.cs
@@ -5784,26 +5784,26 @@ namespace safetyLab.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f020141
-			public const int avd_hide_password_1 = 2130837825;
-			
 			// aapt resource value: 0x7f020142
-			public const int avd_hide_password_2 = 2130837826;
+			public const int avd_hide_password_1 = 2130837826;
 			
 			// aapt resource value: 0x7f020143
-			public const int avd_hide_password_3 = 2130837827;
+			public const int avd_hide_password_2 = 2130837827;
+			
+			// aapt resource value: 0x7f020144
+			public const int avd_hide_password_3 = 2130837828;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020144
-			public const int avd_show_password_1 = 2130837828;
-			
 			// aapt resource value: 0x7f020145
-			public const int avd_show_password_2 = 2130837829;
+			public const int avd_show_password_1 = 2130837829;
 			
 			// aapt resource value: 0x7f020146
-			public const int avd_show_password_3 = 2130837830;
+			public const int avd_show_password_2 = 2130837830;
+			
+			// aapt resource value: 0x7f020147
+			public const int avd_show_password_3 = 2130837831;
 			
 			// aapt resource value: 0x7f020056
 			public const int deakin_burwood = 2130837590;
@@ -6474,11 +6474,11 @@ namespace safetyLab.Droid
 			// aapt resource value: 0x7f020134
 			public const int notification_icon_background = 2130837812;
 			
-			// aapt resource value: 0x7f02013f
-			public const int notification_template_icon_bg = 2130837823;
-			
 			// aapt resource value: 0x7f020140
-			public const int notification_template_icon_low_bg = 2130837824;
+			public const int notification_template_icon_bg = 2130837824;
+			
+			// aapt resource value: 0x7f020141
+			public const int notification_template_icon_low_bg = 2130837825;
 			
 			// aapt resource value: 0x7f020135
 			public const int notification_tile_bg = 2130837813;
@@ -6505,10 +6505,13 @@ namespace safetyLab.Droid
 			public const int Splash = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int tooltip_frame_dark = 2130837821;
+			public const int star = 2130837821;
 			
 			// aapt resource value: 0x7f02013e
-			public const int tooltip_frame_light = 2130837822;
+			public const int tooltip_frame_dark = 2130837822;
+			
+			// aapt resource value: 0x7f02013f
+			public const int tooltip_frame_light = 2130837823;
 			
 			static Drawable()
 			{
